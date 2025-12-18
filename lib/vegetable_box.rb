9797a03db191d4bf1299698bbc3fa203d3278f82
Tobias Matz <matz_tobias@web.de>
2025-12-18 17:29:32 +0100

    Initial Setup

---
+++ lib/vegetable_box.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "vegetable_box/version"
+
+module VegetableBox
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vegetable_box/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module VegetableBox
+  VERSION = "0.1.0"
+end
