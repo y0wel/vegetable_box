7da70de5e4331f8045de1dd10857ae8495a83459
Tobias Matz <matz_tobias@web.de>
2026-01-05 14:39:09 +0100

    Make VegetableBox configuration configurable

--- lib/vegetable_box.rb
+++ lib/vegetable_box.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
+require 'mechanize'
+require 'date'
+require 'json'
+
+require_relative 'vegetable_box/api/authentication'
+require_relative 'vegetable_box/api/base'
+require_relative 'vegetable_box/api/order'
+require_relative 'vegetable_box/client'
+require_relative 'vegetable_box/configuration'
+require_relative 'vegetable_box/error'
+require_relative 'vegetable_box/session'
+require_relative 'vegetable_box/urls'
 require_relative 'vegetable_box/version'
 
 module VegetableBox
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    def config
+      @config ||= VegetableBox::Configuration.new
+    end
+
+    def configure
+      yield(config)
+    end
+  end
 end
--- spec/vegetable_box_spec.rb
+++ spec/vegetable_box_spec.rb
@@ -5,7 +5,19 @@ RSpec.describe VegetableBox do
     expect(VegetableBox::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'is a module carrying a configuration' do
+    expect(described_class.config).to be_a(VegetableBox::Configuration)
+  end
+
+  it 'is a module that can yield the config for initializers to be changed' do
+    tmp_value = described_class.config.username
+
+    expect do
+      described_class.configure do |_config|
+        described_class.config.username = 'new_user'
+      end
+    end.to change { described_class.config.username }.from(tmp_value).to('new_user')
+  ensure
+    described_class.config.username = tmp_value
   end
 end
